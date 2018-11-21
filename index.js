13b99b3581f51d6de9d26f69cafd1b1024a69e3b
kathyleboeuf <kleboeuf@townisp.com>
2018-11-20 20:01:56 -0500

    commit before mapping

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
-const { dbConnect } = require('./db-mongoose');
-// const {dbConnect} = require('./db-knex');
+// const { dbConnect } = require('./db-mongoose');
+const { dbConnect } = require('./db-knex');
 
 const app = express();
 
@@ -21,6 +21,30 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+//==========endpoint
+app.get('/api/cheeses', (req, res) => {
+  res.json([
+    "Bath Blue",
+    "Barkham Blue",
+    "Buxton Blue",
+    "Cheshire Blue",
+    "Devon Blue",
+    "Dorset Blue Vinney",
+    "Dovedale",
+    "Exmoor Blue",
+    "Harbourne Blue",
+    "Lanark Blue",
+    "Lymeswold",
+    "Oxford Blue",
+    "Shropshire Blue",
+    "Stichelton",
+    "Stilton",
+    "Blue Wensleydale",
+    "Yorkshire Blue"
+  ]);
+});
+
+
 
 function runServer(port = PORT) {
   const server = app
